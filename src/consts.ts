7b2c5359c3bffcc5b0787979295229d9d1648986
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-08-20 18:38:56 +0100

    More updates

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,34 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+
+
+export type SocialNetwork = {
+    name: string,
+    url: string,
+    icon: string
+}
+
+export const SOCIALS = [
+    {
+        name: "Twitter",
+        url: "https://twitter.com/feregri_no",
+        icon: "twitter"
+    },
+    {
+        name: "GitHub",
+        url: "",
+        icon: "github"
+    },
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/antonio.feregrino/",
+        icon: "linkedin"
+    },
+    {
+        name: "YouTube",
+        url: "https://www.youtube.com/@feregrino",
+        icon: "youtube"
+    }
+];
